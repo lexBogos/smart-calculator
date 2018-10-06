3640eb02fda3d041dc741b994f9e91f34c01b829
lexBogos <lex.bogos@gmail.com>
2018-10-06 13:35:49 +0300

    solution smart-calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = String(initialValue);  
   }
-
-  add(number) {
-    // your implementation
-  }
-  
+   add(number) {
+      this.initialValue = this.initialValue + '+' + number;
+      return this;
+       }
   subtract(number) {
-    // your implementation
-  }
-
+      this.initialValue = this.initialValue + '-' + number;
+      return this;
+      }
   multiply(number) {
-    // your implementation
-  }
-
+      this.initialValue = this.initialValue + '*' + number;
+      return this;
+      }
   devide(number) {
-    // your implementation
-  }
-
+      this.initialValue = this.initialValue + '/' + number;
+      return this;
+      }
   pow(number) {
-    // your implementation
-  }
-}
-
-module.exports = SmartCalculator;
+      this.initialValue = this.initialValue + '**' + number;
+      return this;
+      }
+  valueOf() {
+    return eval(this.initialValue);
+      }
+ }
+ module.exports = SmartCalculator;
